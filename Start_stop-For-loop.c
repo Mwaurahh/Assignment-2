6fd3afcc7716f73422c49f821661e45f2883ebdd
Mwaurahh <mwaurasamson82@gmail.com>
2024-10-09 19:39:40 +0300

    Start_stop-For-loop.c
    For loop where the user inputs when to start and when to stop

---
+++ Start_stop-For-loop.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+
+int main(){
+    int i,start,stop;
+    
+    printf("Enter when to start:");
+    scanf("%d",&start);
+    
+    printf("Enter when to stop:");
+    scanf("%d",& stop);
+    
+    for(i=start;i<=stop;i++)
+    {   printf("%d\t",i);
+        printf("mwaura\n");
+ 
+    }
+    
+    
+return 0;   
+}
