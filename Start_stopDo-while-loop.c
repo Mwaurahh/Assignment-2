bb8a70b182d9105f49a1e7b7a87f4200e0fcffb3
Mwaurahh <mwaurasamson82@gmail.com>
2024-10-09 19:37:35 +0300

    Start_stopDo-while-loop.c
    A do while loop where the user inputs when to start and when to stop

---
+++ Start_stopDo-while-loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int start,stop;
+    
+       printf("Enter when to start:");
+       scanf("%d",&start);
+    
+      printf("Enter when to stop:");
+      scanf("%d",& stop);
+  
+    
+    
+    do{
+        
+        printf("mwaura\n");
+        start++;
+    }while (start<=stop);
+return 0;   
+}
