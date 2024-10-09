5607a7e8e9a06b99ada8a633c08987212685d157
Mwaurahh <mwaurasamson82@gmail.com>
2024-10-09 19:42:13 +0300

    Start_stopWhile-loop.c
    A while loop where the user has to put when to start and when to stop

---
+++ Start_stopWhile-loop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    
+  int stop,start;
+  
+      
+    printf("Enter when to start:");
+    scanf("%d",&start);
+    
+    printf("Enter when to stop:");
+    scanf("%d",& stop);
+  
+    while (start<=stop)  
+    {  
+        printf("mwaura\n");
+  start++;
+    }
+    
+    return 0;
+}
